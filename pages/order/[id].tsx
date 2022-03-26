8136791aa755abac460bd864112d71d264c5596d
IrvanSN <30742772+IrvanSN@users.noreply.github.com>
2022-03-26 19:27:09 +0700

    Finishing complete checkout page

--- pages/order/[id].tsx
+++ pages/order/[id].tsx
@@ -8,7 +8,8 @@ function OrderId() {
   const router = useRouter();
   const { query, isReady } = router;
   const [dataItem, setDataItem] = useState({
-    _id: '',
+    _id: '', orderId: ''
+
   });
 
   const getOrderDetails = async (orderId: string | string[] | undefined) => {
@@ -29,23 +30,26 @@ function OrderId() {
   }, [isReady]);
 
   return (
-    <div className="grid grid-cols-12 gap-6 mx-8 mt-32 font-poppins lg:px-16 my-10">
-      <div className="col-span-12">
-        <div className="rounded-lg bg-white shadow-md overflow-hidden p-5 hover:shadow-xl transition duration-300">
-          <div className="flex flex-col text-center">
-            <h1 className="mb-2 text-xl font-bold">
-              Silahkan tunjukkan QR Code ini ke kasir!
-            </h1>
-            <hr className="mb-3" />
-            <div className="flex justify-center">
-              <QRCode
-                value={`${process.env.NEXT_PUBLIC_API}/history-order/${dataItem._id}`}
-              />
+    <>
+      <title>{dataItem.orderId} - TITIKOMA</title>
+      <div className="grid grid-cols-12 gap-6 mx-8 mt-32 font-poppins lg:px-16 my-10">
+        <div className="col-span-12">
+          <div className="rounded-lg bg-white shadow-md overflow-hidden p-5 hover:shadow-xl transition duration-300">
+            <div className="flex flex-col text-center">
+              <h1 className="mb-2 text-xl font-bold">
+                Silahkan tunjukkan QR Code ini ke kasir!
+              </h1>
+              <hr className="mb-3" />
+              <div className="flex justify-center">
+                <QRCode
+                  value={`${process.env.NEXT_PUBLIC_API}/history-order/${dataItem._id}`}
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
